3f054a2844300be40a4ea4256f1346aed7200cab
jay <jayesh.sidhwani@gmail.com>
2014-06-08 15:05:21 +0530

    Added procfile and support for heroku

--- api/__init__.py
+++ api/__init__.py
@@ -6,6 +6,7 @@ from flask.ext.restful import Api
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = '99movies'
+app.config['MONGO_URI'] = 'mongodb://jayesh:jay@kahana.mongohq.com:10008/99movies'
 api = Api(app)
 mongo = PyMongo(app)
 
