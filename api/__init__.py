2a9cee7292d8267082802623f69c7e871796276e
jay <jayesh.sidhwani@gmail.com>
2014-06-18 18:09:28 +0530

    Changing HOST

--- api/__init__.py
+++ api/__init__.py
@@ -6,7 +6,7 @@ from flask.ext.restful import Api
 
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = '99movies'
-# app.config['MONGO_URI'] = 'mongodb://jayesh:jayesh@kahana.mongohq.com:10008/99movies'
+app.config['MONGO_URI'] = 'mongodb://jayesh:jayesh@kahana.mongohq.com:10008/99movies'
 api = Api(app)
 mongo = PyMongo(app)
 
